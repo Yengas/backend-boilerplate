0ca2f4682a86aa4be91db1f2e6c1e9ee5e7a6789
Yiğitcan UÇUM <yigitcan@hotmail.com.tr>
2018-02-16 02:24:58 +0300

    add engine for heroku, change LISTEN_PORT env to PORT.

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   host: process.env.APP_HOST || 'localhost',
-  port: process.env.LISTEN_PORT || 8080,
+  port: process.env.PORT || 8080,
   logging: {
     name: 'backend-boilerplate',
     level: 'info'
